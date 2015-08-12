1f558ee52f4cd227b3495f77377597a2865f6f5a
Alexander Bock <abock@ipp.mpg.de>
2015-08-12 10:29:42 +0200

    cleaned up imports

--- lib/settings.py
+++ lib/settings.py
@@ -1,20 +1,6 @@
-import os, sys, numpy as np, matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
-import matplotlib.gridspec as gridspec
-from matplotlib.figure import Figure
-from IPython import embed
-from copy import copy,deepcopy
+import os
+from copy import copy
 import json
-sys.path.append('/afs/ipp/aug/ads-diags/common/python/lib')
-
-import dd_20140805 as dd
-import kk_abock as kk
-sys.path.append('/afs/ipp/home/g/git/python/repository/')
-
-from tooltip import createToolTip
-import fconf
-
-import Tkinter as tk
 
 class Settings(object):
     def __init__(self, filename='%s/.ideview' % os.environ['HOME']):
